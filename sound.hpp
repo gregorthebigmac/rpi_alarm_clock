a7d4a80ef9bd8ede4a6fb97931c3a8ed4506a08b
Gregor <gregorthebigmac@gmail.com>
2018-01-15 14:54:05 -0600

    added sound.hpp
    I decided to make things easier for everyone and make this small class one single source file, instead of breaking it up into a header and source. It's less than 100 lines, and doesn't really warrant splitting up. Also makes the command to compile it a lot simpler.

---
+++ sound.hpp
@@ -0,0 +1,84 @@
+#ifndef SOUND_H
+#define SOUND_H
+
+#include <iostream>
+#include <SDL/SDL.h>
+#include <SDL/SDL_mixer.h>
+
+class sound {
+	sound();	// ctor
+	virtual ~sound();	// dtor
+	
+public:
+	// getters
+	int get_vol() { return m_volume; }
+	int get_vol_increment() { return m_vol_increment; }
+	bool is_playing() { return Mix_PlayingMusic();}
+	
+	// setters
+	void set_vol(int vol);
+	void set_vol_increment(int vol_inc) { m_vol_increment = vol_inc; }
+	void vol_up();
+	void vol_down();
+	
+	// meat & potatoes
+	void play(std::string filename);
+	void stop();
+	void delay(int delay_in_milliseconds) { SDL_Delay(delay_in_milliseconds); }
+
+private:
+	std::string m_audio_file;	// WARNING! 44.1kHz OGG files ONLY! Anything else can cause weird behavior!
+	int m_volume;
+	int m_vol_increment;
+	Mix_Music* m_music;
+};
+
+sound::sound() {
+	if (SDL_Init(SDL_INIT_AUDIO) != 0)
+		std::cerr << "SDL_Init ERROR: " << SDL_GetError() << std::endl;
+	if (Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 2048) != 0)
+		std::cerr << "Mix_OpenAudio ERROR: " << Mix_GetError() << std::endl;
+	set_vol(0);
+	set_vol_increment(10);
+}
+
+void sound::set_vol(int vol) {
+	m_volume = vol;
+	Mix_VolumeMusic(m_volume);
+}
+
+void sound::vol_up() {
+	m_volume = m_volume + m_vol_increment;
+	Mix_VolumeMusic(m_volume);
+}
+
+void sound::vol_down() {
+	m_volume = m_volume - m_vol_increment;
+	Mix_VolumeMusic(m_volume);
+}
+
+void sound::play(std::string filename) {
+	m_audio_file = filename;
+	m_music = Mix_LoadMUS(m_audio_file.c_str());
+	if (m_music) {
+		Mix_VolumeMusic(m_volume);
+		if (Mix_PlayMusic(m_music, 1) != 0) {
+			std::cerr << "Mix_PlayMusic ERROR: " << Mix_GetError() << std::endl;
+			Mix_FreeMusic(m_music);
+		}
+	}
+	else {
+		std::cerr << "Mix_LoadMuS ERROR: " << Mix_GetError() << std::endl;
+		Mix_CloseAudio();
+	}
+}
+
+void sound::stop() {
+	if (is_playing())
+		Mix_HaltMusic();
+	if (m_music)
+		Mix_FreeMusic(m_music);
+	Mix_CloseAudio();
+}
+
+#endif
