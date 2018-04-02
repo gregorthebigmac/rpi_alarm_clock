45adcb5a8d0c2800c2efb946a4f84c5faf81c6bd
Gregor <gregorthebigmac@gmail.com>
2018-04-02 10:42:13 -0500

    Update command.hpp
    Finally updated command.hpp to reflect most recent changes implemented a long time ago.

--- command.hpp
+++ command.hpp
@@ -9,71 +9,72 @@
 #include <vector>
 
 class command {
-
-    public:
-        command();
-        virtual ~command();
-
-        // getters
-        std::vector<std::string>get_terminal_feedback() { return m_terminal_feedback; }
-        std::vector<std::string>get_errors() { return m_errors; }
-        bool still_busy() { return m_still_working; }
-        
-        // meat and potatoes
-        std::string exec(const char *cmd, bool realtime_terminal_output);   // executes a command in the terminal (can run headless by passing FALSE to 2nd arg)
-
-    private:
-        bool m_still_working;   // indicates if the last command entered is still working. Kind of redundant, tbh, because your program can't do anything until it's done, anyway.
-        std::vector<std::string> m_terminal_feedback;   // all console output is returned line-by-line as a vector of strings
-        std::vector<std::string> m_errors;              // any errors returned by the PIPE will be stored here (this does NOT include errors from your command, itself. ONLY PIPE errors)
+public:
+    command();
+    virtual ~command();        
+    
+	// main function. Fire and forget. Takes two args. command to execute as c_str, and bool: TRUE to print terminal output in realtime, FALSE to run quiet (or headless)
+    void exec(const char *cmd, bool realtime_terminal_feedback);
+	// overloaded main function. Takes additional two args, both are vector<string> for storing terminal feedback and any errors that may arise.
+	void exec(const char *cmd, std::vector<std::string> &terminal_output, std::vector<std::string> &error_list, bool realtime_terminal_feedback);
 };
 
-command::command() {
-    m_still_working = false;    // default. Keeps track of whether or not it's currently executing a command or still getting feedback from the terminal.
-}
+command::command() {}
 
 command::~command() {}
 
-std::string command::exec(const char* cmd, bool realtime_terminal_output) {
-    m_terminal_feedback.clear();
-    m_errors.clear();
-    if (!still_busy()) {
-        m_still_working = true;
-        char buffer[128];
-        FILE* pipe = popen(cmd, "r");
-        if (!pipe) {
-            m_errors.push_back("popen() failed!");
-            if (realtime_terminal_output)
-            	std::cout << "popen() failed!" << std::endl;
-            throw::std::runtime_error("popen() failed!");
-        }
+void exec(const char *cmd, bool realtime_terminal_feedback) {
+	char buffer[128];
+	FILE *_pipe = popen(cmd, "r");
+	if (!_pipe) {
+		if (realtime_terminal_feedback)
+			std::cout << "popen() failed!" << std::endl;
+		throw ::std::runtime_error("popen() failed!");
         try {
-            while (!feof(pipe)) {
-                if (fgets(buffer, 128, pipe) != NULL) {
+            while (!feof(_pipe)) {
+                if (fgets(buffer, 128, _pipe) != NULL) {
                 	std::string temp = buffer;
-                    m_terminal_feedback.push_back(temp);
-                    if (realtime_terminal_output)
-                    	std::cout << temp;
+					if (realtime_terminal_feedback)
+						std::cout << temp;
                 }
             }
         } catch (...) {
-            pclose(pipe);
-            m_still_working = false;
-            m_errors.push_back("fgets failed!");
-            if (realtime_terminal_output)
-            	std::cout << "fgets failed!" << std::endl;
+            pclose(_pipe);
+            if (realtime_terminal_feedback)
+            	std::cout << "unknown error" << std::endl;
             throw;
-            return "fgets failed!";
+        	std::cout << "Failed to fetch data from FILE *_pipe!";
         }
-        pclose(pipe);
-        m_still_working = false;
-        return "ok";
+        pclose(_pipe);
     }
-    else {
-        m_errors.push_back("busy. Try again later.");
-        if (realtime_terminal_output)
-        	std::cout << "busy. Try again later." << std::endl;
-        return "busy";
+}
+
+void command::exec(const char *cmd, std::vector<std::string> &terminal_output, std::vector<std::string> &error_list, bool realtime_terminal_feedback) {
+	char buffer[128];
+	FILE *_pipe = popen(cmd, "r");
+	if (!_pipe) {
+		error_list.push_back("popen() failed!");
+		if (realtime_terminal_feedback)
+			std::cout << "popen() failed!" << std::endl;
+		throw ::std::runtime_error("popen() failed!");
+        try {
+            while (!feof(_pipe)) {
+                if (fgets(buffer, 128, _pipe) != NULL) {
+                	std::string temp = buffer;
+                    terminal_output.push_back(temp);
+					if (realtime_terminal_feedback)
+						std::cout << temp;
+                }
+            }
+        } catch (...) {
+            pclose(_pipe);
+            error_list.push_back("Failed to fetch data from FILE *_pipe!");
+            if (realtime_terminal_feedback)
+            	std::cout << "unknown error" << std::endl;
+            throw;
+        	std::cout << "Failed to fetch data from FILE *_pipe!";
+        }
+        pclose(_pipe);
     }
 }
 
