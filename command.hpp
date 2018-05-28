64102e6cff143de1dcfd204a0f8086d0f0aa457d
Gregor <gregorthebigmac@gmail.com>
2018-05-28 00:40:01 -0500

    fixed command.hpp
    During my last patch, this file got screwed up somehow. A couple of braces and statements got out of place. Tested and confirmed working.

--- command.hpp
+++ command.hpp
@@ -10,71 +10,67 @@
 
 class command {
 public:
-	command();
-	virtual ~command();        
-    
-	// main function. Fire and forget. Takes two args. command to execute as c_str, and bool: TRUE to print terminal output in realtime, FALSE to run quiet (or headless)
-	void exec(const char *cmd, bool realtime_terminal_feedback);
-	// overloaded main function. Takes additional two args, both are vector<string> for storing terminal feedback and any errors that may arise.
-	void exec(const char *cmd, std::vector<std::string> &terminal_output, std::vector<std::string> &error_list, bool realtime_terminal_feedback);
+    command();
+    virtual ~command();
+    // Fire and forget. Takes a terminal command as first arg, and set 2nd arg bool to true to see terminal output in real time, false for quiet.
+    void exec(const char *cmd, bool realtime_terminal_feedback);
+    // overloaded method. Takes two vectors of strings as args, and pushes all terminal output to the first one, and errors reported to the second one. same rules for original two args holds.
+    void exec(const char *cmd, std::vector<std::string> &terminal_output, std::vector<std::string> &error_list, bool realtime_terminal_feedback);
 };
 
-command::command() {}
-command::~command() {}
+command::command() {}   // ctor
+command::~command() {}  // dtor
 
-void exec(const char *cmd, bool realtime_terminal_feedback) {
-	char buffer[128];
-	FILE *_pipe = popen(cmd, "r");
-	if (!_pipe) {
-		if (realtime_terminal_feedback)
-			std::cout << "popen() failed!" << std::endl;
-		throw ::std::runtime_error("popen() failed!");
-		try {
-			while (!feof(_pipe)) {
-				if (fgets(buffer, 128, _pipe) != NULL) {
-					std::string temp = buffer;
-					if (realtime_terminal_feedback)
-						std::cout << temp;
-				}
-			}
-		} catch (...) {
-			pclose(_pipe);
-			if (realtime_terminal_feedback)
-				std::cout << "unknown error" << std::endl;
-			throw;
-			std::cout << "Failed to fetch data from FILE *_pipe!";
-		}
-		pclose(_pipe);
-	}
+void command::exec(const char* cmd, bool realtime_terminal_feedback) {
+    char buffer[128];
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) {
+        if (realtime_terminal_feedback)
+            std::cout << "popen() failed!" << std::endl;
+        throw::std::runtime_error("popen() failed!");
+    }
+    try {
+        while (!feof(pipe)) {
+            if (fgets(buffer, 128, pipe) != NULL) {
+                if (realtime_terminal_feedback)
+                    std::cout << buffer;
+            }
+        }
+    } catch (...) {
+        pclose(pipe);
+        if (realtime_terminal_feedback)
+            std::cout << "fgets failed!" << std::endl;
+        throw;
+    }
+    pclose(pipe);
 }
 
-void command::exec(const char *cmd, std::vector<std::string> &terminal_output, std::vector<std::string> &error_list, bool realtime_terminal_feedback) {
-	char buffer[128];
-	FILE *_pipe = popen(cmd, "r");
-	if (!_pipe) {
-		error_list.push_back("popen() failed!");
-		if (realtime_terminal_feedback)
-			std::cout << "popen() failed!" << std::endl;
-		throw ::std::runtime_error("popen() failed!");
-		try {
-			while (!feof(_pipe)) {
-				if (fgets(buffer, 128, _pipe) != NULL) {
-					std::string temp = buffer;
-					terminal_output.push_back(temp);
-					if (realtime_terminal_feedback)
-						std::cout << temp;
-				}
-			}
-		} catch (...) {
-			pclose(_pipe);
-			error_list.push_back("Failed to fetch data from FILE *_pipe!");
-			if (realtime_terminal_feedback)
-				std::cout << "unknown error" << std::endl;
-			throw;
-			std::cout << "Failed to fetch data from FILE *_pipe!";
-		}
-		pclose(_pipe);
-	}
+void command::exec(const char* cmd, std::vector<std::string> &terminal_output, std::vector<std::string> &error_list, bool realtime_terminal_feedback) {
+    char buffer[128];
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) {
+        error_list.push_back("popen() failed!");
+        if (realtime_terminal_feedback)
+            std::cout << "popen() failed!" << std::endl;
+        throw::std::runtime_error("popen() failed!");
+    }
+    try {
+        while (!feof(pipe)) {
+            if (fgets(buffer, 128, pipe) != NULL) {
+                std::string temp = buffer;
+                terminal_output.push_back(temp);
+                if (realtime_terminal_feedback)
+                    std::cout << temp;
+            }
+        }
+    } catch (...) {
+        pclose(pipe);
+        error_list.push_back("fgets failed!");
+        if (realtime_terminal_feedback)
+            std::cout << "fgets failed!" << std::endl;
+        throw;
+    }
+    pclose(pipe);
 }
 
 #endif // COMMAND_HPP
